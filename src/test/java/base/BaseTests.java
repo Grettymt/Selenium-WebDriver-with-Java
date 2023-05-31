a18e55ef2e4bf9a823638929e9de2e4127e1c99f
Gretty Mosquera <gretymosquera@gmail.com>
2023-05-31 13:02:09 -0500

    Se agrega interacción con localizadores [Links]

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,9 +1,13 @@
 package base;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
 
 public class BaseTests {
     private WebDriver webDriver;
@@ -15,6 +19,20 @@ public class BaseTests {
         webDriver = new ChromeDriver(co);
         webDriver.get("https://the-internet.herokuapp.com/");
         webDriver.manage().window().maximize();
+
+        /*
+        WebElement inputsLink = webDriver.findElement(By.linkText("Inputs"));
+        inputsLink.click();
+*/
+        WebElement shiftingCont = webDriver.findElement(By.linkText("Shifting Content"));
+        shiftingCont.click();
+
+        WebElement menu = webDriver.findElement(By.linkText("Example 1: Menu Element"));
+        menu.click();
+
+        List<WebElement> links = webDriver.findElements(By.tagName("li"));
+        System.out.println("Cantidad de links: " + links.size());
+
         System.out.println(webDriver.getTitle());
         webDriver.quit();
     }
