7a4aff12c02d7fa764ec514c83690ee1f9e7589e
Gretty Mosquera <gretymosquera@gmail.com>
2023-06-23 13:02:49 -0500

    Added documentation to the DropdownPage class

--- src/main/java/pages/DropdownPage.java
+++ src/main/java/pages/DropdownPage.java
@@ -29,16 +29,29 @@ public class DropdownPage {
         this.webDriver = webDriver;
     }
 
+    /**
+     * Method that allows you to select a specific option from a dropdown menu
+     * @param option
+     */
     public void selectFromDropDown(String option){
         findDropElement().selectByVisibleText(option);
     }
 
+    /**
+     * This method retrieves the selected options from a dropdown list item and returns them as a list of text strings
+     * @return
+     */
     public List<String> getSelectedOptions(){
         List<WebElement> selectedElements =
                 findDropElement().getAllSelectedOptions();
         return selectedElements.stream().map(e->e.getText()).collect(Collectors.toList());
     }
 
+    /**
+     * A method that allows the drop-down list element to be encapsulated in an object of type Select, which makes
+     * it easier to interact with the operations related to drop-down lists
+     * @return
+     */
     private Select findDropElement(){
         return new Select(webDriver.findElement(dropdown));
     }
